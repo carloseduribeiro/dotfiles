e53f99dbd8a9348e8da0ec28cbbddce955da7804
carloseduribeiro <carloseribeiro96@gmail.com>
2024-10-22 21:37:53 -0300

    Improving LSP plugins config in file lua/plugins/lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,28 +1,23 @@
 return {
-  { "neovim/nvim-lspconfig" },
-  { "hrsh7th/cmp-nvim-lsp" },
-  { "hrsh7th/nvim-cmp"},
-  { "williamboman/mason-lspconfig.nvim" },
-  { "williamboman/mason.nvim" },
+  -- Plugins LSP + Mason
   {
     "VonHeikemen/lsp-zero.nvim",
     branch = "v4.x",
     dependencies = {
-      -- LSP Support
       { "neovim/nvim-lspconfig" }, -- Required
-      { -- Optional
+      {
         "williamboman/mason.nvim",
         build = function()
-          pcall(vim.cmd, "MasonUpdate")
+          vim.cmd("MasonUpdate")
         end,
       },
-      { "williamboman/mason-lspconfig.nvim" }, -- Optional
-  
+      { "williamboman/mason-lspconfig.nvim" }, -- Integração do Mason com lspconfig
+
       -- Autocompletion
       { "hrsh7th/nvim-cmp" }, -- Required
       { "hrsh7th/cmp-nvim-lsp" }, -- Required
       { "L3MON4D3/LuaSnip" }, -- Required
-      { "rafamadriz/friendly-snippets" },
+      { "rafamadriz/friendly-snippets" }, -- Snippets pré-configurados
       { "hrsh7th/cmp-buffer" },
       { "hrsh7th/cmp-path" },
       { "hrsh7th/cmp-cmdline" },
@@ -30,35 +25,47 @@ return {
     },
     config = function()
       local lsp = require("lsp-zero")
-  
-      lsp.on_attach(function(client, bufnr)
+
+      local on_attach = function(client, bufnr)
         local opts = { buffer = bufnr, remap = false }
-  
-        vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, vim.tbl_deep_extend("force", opts, { desc = "LSP Goto Reference" }))
-        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, vim.tbl_deep_extend("force", opts, { desc = "LSP Goto Definition" }))
-        vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, vim.tbl_deep_extend("force", opts, { desc = "LSP Hover" }))
-        vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, vim.tbl_deep_extend("force", opts, { desc = "LSP Workspace Symbol" }))
-        vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.setloclist() end, vim.tbl_deep_extend("force", opts, { desc = "LSP Show Diagnostics" }))
-        vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, vim.tbl_deep_extend("force", opts, { desc = "Next Diagnostic" }))
-        vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, vim.tbl_deep_extend("force", opts, { desc = "Previous Diagnostic" }))
-        vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, vim.tbl_deep_extend("force", opts, { desc = "LSP Code Action" }))
-        vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, vim.tbl_deep_extend("force", opts, { desc = "LSP References" }))
-        vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, vim.tbl_deep_extend("force", opts, { desc = "LSP Rename" }))
-        vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, vim.tbl_deep_extend("force", opts, { desc = "LSP Signature Help" }))
-      end)
-  
+        local keymap = vim.keymap.set
+        local buf = vim.lsp.buf
+        local diagnostic = vim.diagnostic
+
+        keymap("n", "gr", buf.references, vim.tbl_extend("force", opts, { desc = "LSP Goto Reference" }))
+        keymap("n", "gd", buf.definition, vim.tbl_extend("force", opts, { desc = "LSP Goto Definition" }))
+        keymap("n", "K", buf.hover, vim.tbl_extend("force", opts, { desc = "LSP Hover" }))
+        keymap("n", "<leader>vws", buf.workspace_symbol, vim.tbl_extend("force", opts, { desc = "LSP Workspace Symbol" }))
+        keymap("n", "<leader>vd", diagnostic.setloclist, vim.tbl_extend("force", opts, { desc = "LSP Show Diagnostics" }))
+        keymap("n", "[d", diagnostic.goto_next, vim.tbl_extend("force", opts, { desc = "Next Diagnostic" }))
+        keymap("n", "]d", diagnostic.goto_prev, vim.tbl_extend("force", opts, { desc = "Previous Diagnostic" }))
+        keymap("n", "<leader>vca", buf.code_action, vim.tbl_extend("force", opts, { desc = "LSP Code Action" }))
+        keymap("n", "<leader>vrn", buf.rename, vim.tbl_extend("force", opts, { desc = "LSP Rename" }))
+        keymap("i", "<C-h>", buf.signature_help, vim.tbl_extend("force", opts, { desc = "LSP Signature Help" }))
+      end
+
       require("mason").setup({})
       require("mason-lspconfig").setup({
         ensure_installed = {
+	  "marksman",
+	  "jsonls",
+	  "pylsp",
+	  "dockerls",
+	  "bashls",
           "gopls",
         },
         handlers = {
           lsp.default_setup,
+          -- Configuração específica para Language Servers:
           lua_ls = function()
             local lua_opts = lsp.nvim_lua_ls()
             require("lspconfig").lua_ls.setup(lua_opts)
           end,
         },
+      })
+
+      lsp.setup({
+        on_attach = on_attach,
       })
     end,
   }
