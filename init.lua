c1b6c4304ac7119976bbf821a66da54cce3d9b37
carloseduribeiro <carloseribeiro96@gmail.com>
2024-10-25 00:57:47 -0300

    Finishing configuration based on kickstat

---
+++ init.lua
@@ -0,0 +1,29 @@
+require 'core.options'
+require 'core.keymaps'
+
+-- [[ Install `lazy.nvim` plugin manager ]]
+--    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+    local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+    if vim.v.shell_error ~= 0 then
+        error('Error cloning lazy.nvim:\n' .. out)
+    end
+end
+vim.opt.rtp:prepend(lazypath)
+
+require('lazy').setup {
+    require 'plugins.neo-tree',
+    require 'plugins.colorthemes',
+    require 'plugins.bufferline',
+    require 'plugins.lualine',
+    require 'plugins.treesitter',
+    require 'plugins.telescope',
+    require 'plugins.lsp',
+    require 'plugins.autocompletion',
+    require 'lua.plugins.autoformatting',
+    require 'lua.plugins.gitsigns',
+    require 'lua.plugins.alpha',
+    require 'lua.plugins.indent-blankline',
+}
