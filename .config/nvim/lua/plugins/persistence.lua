a83f5a9609edd8b549bfcdf9efabbdc234ee93bd
carloseduribeiro <carloseribeiro96@gmail.com>
2024-11-16 00:48:55 -0300

    configure persistence

--- .config/nvim/lua/plugins/persistence.lua
+++ .config/nvim/lua/plugins/persistence.lua
@@ -1,25 +1,59 @@
 return {
     'folke/persistence.nvim',
-    event = 'BufReadPre', -- this will only start session saving when an actual file was opened
-    config = function()
-        -- load the session for the current directory
-        vim.keymap.set('n', '<leader>qs', function()
-            require('persistence').load()
-        end)
-
-        -- select a session to load
-        vim.keymap.set('n', '<leader>qS', function()
-            require('persistence').select()
-        end)
-
-        -- load the last session
-        vim.keymap.set('n', '<leader>ql', function()
-            require('persistence').load { last = true }
-        end)
-
-        -- stop Persistence => session won't be saved on exit
-        vim.keymap.set('n', '<leader>qd', function()
-            require('persistence').stop()
-        end)
+    event = 'VimEnter',
+    opts = {
+        dir = vim.fn.stdpath 'state' .. '/sessions/',
+        branch = false,
+        -- Enable to autoload session on startup, unless:
+        -- * neovim was started with files as arguments
+        -- * stdin has been provided
+        -- * git commit/rebase session
+        autoload = true,
+    },
+		-- stylua: ignore
+		keys = {
+			{ '<leader>qs', function() require('persistence').load() end, desc = 'Restore Session' },
+			{ '<leader>ql', function() require('persistence').load({ last = true }) end, desc = 'Restore Last Session' },
+            { '<leader>qd', function() require('persistence').stop() end, desc = 'Don\'t Save Current Session' },
+		},
+    init = function()
+        -- Detect if stdin has been provided.
+        vim.g.started_with_stdin = false
+        vim.api.nvim_create_autocmd('StdinReadPre', {
+            group = vim.api.nvim_create_augroup('c4du_persistence', {}),
+            callback = function()
+                vim.g.started_with_stdin = true
+            end,
+        })
+        -- Autoload session on startup.
+        local disabled_dirs = {
+            vim.env.TMPDIR or '/tmp',
+            '/private/tmp',
+        }
+        vim.api.nvim_create_autocmd('VimEnter', {
+            group = 'c4du_persistence',
+            once = true,
+            nested = true,
+            callback = function()
+                local cwd = vim.uv.cwd() or vim.fn.getcwd()
+                if cwd == nil or vim.fn.argc() > 0 or vim.g.started_with_stdin or vim.env.GIT_EXEC_PATH ~= nil then
+                    require('persistence').stop()
+                    return
+                end
+                for _, path in pairs(disabled_dirs) do
+                    if cwd:sub(1, #path) == path then
+                        require('persistence').stop()
+                        return
+                    end
+                end
+                -- Close all floats before loading a session. (e.g. Lazy.nvim)
+                for _, win in pairs(vim.api.nvim_tabpage_list_wins(0)) do
+                    if vim.api.nvim_win_get_config(win).zindex then
+                        vim.api.nvim_win_close(win, false)
+                    end
+                end
+                require('persistence').load()
+            end,
+        })
     end,
 }
